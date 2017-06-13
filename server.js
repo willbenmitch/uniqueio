e1b416a5736f5bbf3424025f4daae8bddab1192f
willbenmitch <ben.mitchell01@gmail.com>
2017-06-12 22:51:16 -0400

    add NODE_ENV to process.env spec

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req,res, next) => {
     next();
 });
 // USE PUBLIC DIRECTORY
-if (process.env === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(__dirname+'/build'));
 } else {
     app.use(express.static(__dirname+'/public'));
